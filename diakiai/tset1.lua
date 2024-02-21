112e07e521c90b4f277121713c1ee042871f1002
palacetamon098 <158472954+palacetamon098@users.noreply.github.com>
2024-02-21 15:05:54 +0700

    Update tset1.lua

--- diakiai/tset1.lua
+++ diakiai/tset1.lua
@@ -1,8 +1,4 @@
---[[ ui:
-    fluent
-    SaveManager
-    InterfaceManager
--- ..loading]]
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
